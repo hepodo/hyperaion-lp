f8a7c6d42c74458168ba2100b5afc3cb097171d3
HyperAION <hyperaion.ai+info@gmail.com>
2026-05-01 11:57:09 +0900

    feat: update HyperAION canonical LP URL

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,24 @@
+const LEGACY_PREFIX = '/hyperaion/lp';
+const CANONICAL_PREFIX = '/hyperaion';
+const PERMANENT_REDIRECT = 301;
+
+function buildCanonicalUrl(requestUrl) {
+  const url = new URL(requestUrl);
+  url.pathname = url.pathname.replace(LEGACY_PREFIX, CANONICAL_PREFIX);
+  if (url.pathname === CANONICAL_PREFIX) url.pathname = `${CANONICAL_PREFIX}/`;
+  return url;
+}
+
+function shouldRedirect(pathname) {
+  return pathname === LEGACY_PREFIX || pathname.startsWith(`${LEGACY_PREFIX}/`);
+}
+
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    let pathname = url.pathname;
-
-    if (pathname.endsWith('/')) {
-      pathname += 'index.html';
+    if (shouldRedirect(url.pathname)) {
+      return Response.redirect(buildCanonicalUrl(request.url), PERMANENT_REDIRECT);
     }
-
-    const assetUrl = new URL(pathname, url.origin);
-    return env.ASSETS.fetch(new Request(assetUrl, request));
-  }
+    return env.ASSETS.fetch(request);
+  },
 };
